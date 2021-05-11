7d0630019ee563f999566435e601cd9e7687b2b1
Graham Burgess <grahammb_315@hotmail.co.uk>
2021-05-11 15:40:45 +0100

    purple points

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color="blue") +
+  geom_point(color="purple") +
   theme_minimal()
 
 # Save the plot in graphics/
