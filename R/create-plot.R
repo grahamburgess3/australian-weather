511632566292ea6afedafe823c664a5d303fa646
Graham Burgess <grahammb_315@hotmail.co.uk>
2021-05-11 13:48:06 +0100

    Theme minimal

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color="blue")
+  geom_point(color="blue") +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
